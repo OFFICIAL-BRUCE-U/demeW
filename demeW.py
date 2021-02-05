e6f61611407f658affce131620ade6f2d42560ec
OFFICIAL-BRUCE-U <78590961+OFFICIAL-BRUCE-U@users.noreply.github.com>
2021-02-05 09:17:12 +0000

    Add files via upload

---
+++ demeW.py
@@ -0,0 +1,10 @@
+print("demeLar have you graduated yet Maria?")
+
+graduated = input()
+if graduated == "yes":
+    print("demeW guys i'm smart, I have a degree")
+elif graduated == "no":
+    print("guess you guys were right FeelsWeakMan")
+else:
+    print("demeDank HUH?")
+
